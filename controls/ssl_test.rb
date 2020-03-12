3da3323d611f17bd2fc19ddd43b4ed3159378fda
Michée lengronne <michee.lengronne@coppint.com>
2020-03-12 23:20:08 +0100

    debug force_ssl
    the previous force_ssl mechanism didn't work. This one does.
    
    Signed-off-by: Michée Lengronne <michee.lengronne@coppint.com>

--- controls/ssl_test.rb
+++ controls/ssl_test.rb
@@ -62,7 +62,11 @@ end
 
 # Filter out ports that don't respond to any version of SSL
 sslports = tcpports.find_all do |tcpport|
-  !exclude_ports.include?(tcpport[:port]) && (ssl(tcpport).enabled? unless force_ssl)
+  if force_ssl
+    !exclude_ports.include?(tcpport[:port])
+  else
+    !exclude_ports.include?(tcpport[:port]) && ssl(tcpport).enabled?
+  end
 end
 
 # Troubleshooting control to show InSpec version and list
